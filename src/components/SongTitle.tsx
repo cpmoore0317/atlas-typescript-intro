a0875d2525dfb940d996a76e1df6d5405643d77e
cpmoore0317 <cpmoore317@gmail.com>
2024-09-11 19:14:52 -0500

    Changing SongTitle component to typescript

---
+++ src/components/SongTitle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type SongTitleProps = {
+  title: string;
+  author: string;
+};
+
+const SongTitle: React.FC<SongTitleProps> = ({ title, author }) => {
+  return (
+    <div>
+      <h2 className="text-lg font-bold">{title}</h2>
+      <p className="text-sm text-gray-500">{author}</p>
+    </div>
+  );
+};
+
+export default SongTitle;
